f967cc75ce7fe0e14508fdeb562cf6b6c8d389af
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-16 02:29:53 +0530

    Add files via upload
    while--loop

---
+++ Question1.java
@@ -0,0 +1,24 @@
+
+// 1.write a program to print 25 to 50 numbers using while loop.
+
+
+
+
+
+
+public class Question1 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+	
+		int i = 25;
+		while (i<=50)
+		{
+			System.out.println("-->"+ i);
+			i++;
+		}
+		
+
+	}
+
+}
